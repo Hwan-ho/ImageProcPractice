fa8b88f0b6f4a54f45930214fad9ab88758ac628
Hwan-ho <nara9313@gmail.com>
2024-06-20 18:52:32 +0900

    Update Chat2.BitDepth.py

--- Material/Chat2.BitDepth.py
+++ Material/Chat2.BitDepth.py
@@ -5,7 +5,7 @@ import numpy as np
 image = np.array(Image.open('/public/pythonPractice/lena_gray.jpeg'))
 
 bitdepth = 3
-image_bitdepth = image//(256//2**bitdepth)
+image_bitdepth = image>>(8-bitdepth)
 
 plt.figure(figsize=(16,8))
 plt.subplot(1,2,1)
